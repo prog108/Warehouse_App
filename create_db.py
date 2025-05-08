1c606adb3ca5f67b2b6b8a1cedc84f2fbf4d8278
prog108 <prog100@yahoo.com>
2025-05-08 17:03:29 +0200

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -9,9 +9,7 @@ CREATE TABLE Foodstufs(
 Name TEXT PRIMARY KEY,
 Category TEXT NOT NULL,
 Quantity INTEGER NOT NULL,
-Location TEXT NOT NULL,
-Minimum_stock_level INTEGER NOT NULL,
-Maximum_stock_level INTEGER NOT NULL
+Location TEXT NOT NULL
 )
 '''
 
